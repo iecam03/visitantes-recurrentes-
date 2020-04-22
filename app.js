c58bacb8527bfb0d9900db55959e6ac6e78418e4
Camilo <iecam03@gmail.com>
2020-04-21 19:38:53 -0500

    recurrentes 1

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/test', {
+  useNewUrlParser: true, useUnifiedTopology: true
+});
+mongoose.connection.on("error", function (e) { console.error(e); });
+
+
+var visitorSchema = mongoose.Schema({
+  name: String,
+  count: Number,
+});
+const Visitor = mongoose.model("Visitors", visitorSchema);
+
+app.get('/', (req, res) => {
+  var name = req.query.name;
+  console.log("name", name);
+
+  Visitor.updateMany({ name: name }, {$inc: { count: 1 }}, function (err, visit) {
+    if (err) return console.error(err);
+    console.log(visit);
+    if (visit.n === 0) {
+      Visitor.create({ name: name || "Anónimo", count: 1 });
+    }
+    let HTML = "";
+    HTML += '<table><tbody class="tab">';
+    HTML += '<tr><th>ID</th><th>Name</th><th>Visits</th></tr>';
+
+    Visitor.find(function (err, visitors) {
+      if (err) return console.log(err)
+
+      visitors.forEach(function (vi) {
+        HTML += '<tr><td>' + vi["_id"] + '</td><td>' + vi["name"] + '</td><td>' + vi["count"] + '</td></tr>';
+      });
+      HTML += '</tbody></table>';
+      res.send(HTML)
+    });
+  });
+});
+
+
+  app.listen(3000, () => console.log('Listening on port 3000!'));
+
